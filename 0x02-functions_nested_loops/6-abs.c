2d562425a667dc3f8e812a1eca86af56a109a3ed
Betelhemmesele <betimesele93@gmail.com>
2022-03-15 23:26:45 +0300

    my commit message

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - a function that computes the absolute value of an integer
+ * @ab: integer input
+ * Return: absolute value of ab
+ */
+int _abs(int ab)
+{
+	if (ab >= 0)
+	{
+		_putchar('ab');
+	}
+	else
+	{
+		ab = -1 * ab;
+		_putchar('ab');
+	}
+}
+
+
